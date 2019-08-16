4807cdc0122e9efe551b819ad60f486374ccea77
lockdawg <43064175+lockdawg@users.noreply.github.com>
2019-08-16 09:25:46 -0700

    Update bcz_install.sh

--- bcz_install.sh
+++ bcz_install.sh
@@ -5,7 +5,7 @@ CONFIG_FILE='bcz.conf'
 CONFIGFOLDER='/root/.bcz'
 COIN_DAEMON='/usr/local/bin/bczd'
 COIN_CLI='/usr/local/bin/bcz-cli'
-COIN_REPO='https://github.com/BitcoinCZ/bitcoincz/releases/download/6.0.0.8/linux-x64.tar.gz'
+COIN_REPO='https://github.com/BitcoinCZ/bitcoincz/releases/download/6.0.0.9/linux-x64.tar.gz'
 COIN_NAME='BCZ'
 COIN_RPC=29501
 COIN_PORT=29500
@@ -27,8 +27,8 @@ function compile_node() {
   COIN_ZIP=$(echo $COIN_REPO | awk -F'/' '{print $NF}')
   tar xvf linux-x64.tar.gz
   compile_error
-  sudo cp bcz-cli /usr/local/bin
-  sudo cp bczd /usr/local/bin
+  cp bcz-cli /usr/local/bin
+  cp bczd /usr/local/bin
   compile_error
   strip $COIN_DAEMON $COIN_CLI
   cd - >/dev/null 2>&1
@@ -43,24 +43,19 @@ function configure_systemd() {
 [Unit]
 Description=$COIN_NAME service
 After=network.target
-
 [Service]
 User=root
 Group=root
-
 Type=forking
 #PIDFile=$CONFIGFOLDER/$COIN_NAME.pid
-
 ExecStart=$COIN_DAEMON -daemon -conf=$CONFIGFOLDER/$CONFIG_FILE -datadir=$CONFIGFOLDER
 ExecStop=-$COIN_CLI -conf=$CONFIGFOLDER/$CONFIG_FILE -datadir=$CONFIGFOLDER stop
-
 Restart=always
 PrivateTmp=true
 TimeoutStopSec=60s
 TimeoutStartSec=10s
 StartLimitInterval=120s
 StartLimitBurst=5
-
 [Install]
 WantedBy=multi-user.target
 EOF
@@ -175,7 +170,7 @@ function compile_error() {
 
 
 function checks() {
-  if [[ $(lsb_release -d) != *18.04* ]]; then
+  if [[ $(lsb_release -d) != *16.04* ]]; then
     echo -e "${RED}You are not running Ubuntu 16.04. Installation is cancelled.${NC}"
     exit 1
   fi
@@ -266,7 +261,7 @@ function setup_node() {
 ##### Main #####
 clear
 
-#checks
-#prepare_system
+checks
+prepare_system
 compile_node
 setup_node
